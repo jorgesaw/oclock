e9afaebaa469dad553fe76613d4ef2127bf287fb
jorgesaw <jorgesaw@gmail.com>
2020-06-14 02:06:49 -0300

    Source code base without apps

---
+++ config/settings/base.py
@@ -0,0 +1,268 @@
+"""Base settings to build other settings files upon."""
+
+import os
+import environ
+
+from django.utils.translation import gettext_lazy as _
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ROOT_DIR = environ.Path(__file__) - 3
+APPS_DIR = ROOT_DIR.path('apps')
+
+env = environ.Env(
+    DEBUG=(bool, False)    
+)
+environ.Env.read_env()
+
+# Base
+DEBUG = env.bool('DJANGO_DEBUG', False)
+
+# Language and timezone
+TIME_ZONE = 'America/Buenos_Aires'
+LANGUAGE_CODE = 'es-ar'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# DATABASES
+DATABASES = {
+    'default': env.db('DATABASE_URL'),
+}
+
+DATABASES['default']['ATOMIC_REQUESTS'] = True
+
+# URLs
+ROOT_URLCONF = 'config.urls'
+
+# WSGI
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# Users & Authentication
+AUTH_USER_MODEL = 'users.User'
+
+# Apps
+DJANGO_APPS = [
+    'apps.registration.apps.RegistrationAppConfig', # Prioritize at auth.
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'extra_views',
+]
+
+LOCAL_APPS = [
+    'apps.core.apps.CoreAppConfig',
+    'apps.users.apps.UsersAppConfig',
+    'apps.profiles.apps.ProfilesAppConfig',
+    'apps.inventories.apps.InventoriesAppConfig',
+    'apps.locations.apps.LocationsAppConfig',
+#    'apps.locations.apps.PersonsAppConfig',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# Passwords
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Middlewares
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Static files
+STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    str(APPS_DIR.path('static')),
+]
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+# Media
+MEDIA_ROOT = str(APPS_DIR('media'))
+MEDIA_URL = '/media/'
+
+# Templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            str(APPS_DIR.path('templates')),
+        ],
+        'OPTIONS': {
+            'debug': DEBUG,
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ],
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+LOCALE_PATHS = (
+    os.path.join(ROOT_DIR, 'locale'),
+)
+LANGUAGES = [
+    ('es', _('Spanish')),
+    ('en', _('English')),
+]
+
+# Security
+SESSION_COOKIE_HTTPONLY = True
+CSRF_COOKIE_HTTPONLY = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Email
+EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+
+# Admin
+ADMIN_URL = 'admin/'
+ADMINS = [
+    ("""Jorge Gonzalez""", 'jorgesaw@gmail.com'),
+]
+MANAGERS = ADMINS
+
+# Auths redirects
+LOGIN_URL = '/accounts/login/'
+# LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'core:home'
+
+# Celery
+"""
+INSTALLED_APPS += ['cride.taskapp.celery.CeleryAppConfig']
+if USE_TZ:
+    CELERY_TIMEZONE = TIME_ZONE
+
+CELERY_BROKER_URL = env('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERYD_TASK_TIME_LIMIT = 5 * 60
+CELERYD_TASK_SOFT_TIME_LIMIT = 60
+"""
+# Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+}
+
+# Django filters
+
+def FILTERS_VERBOSE_LOOKUPS():
+    from django_filters.conf import DEFAULTS
+
+    verbose_lookups = DEFAULTS['VERBOSE_LOOKUPS'].copy()
+    verbose_lookups.update({
+        'gt': 'mayor que', 
+        'gte': 'mayor o igual que',
+        'lt': 'menor que', 
+        'lte': 'menor o igual que',
+        'contains': '', 
+        'icontains': '' 
+    })
+    return verbose_lookups
+
+# Logging
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See https://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+"""
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True
+        },
+        'django.security.DisallowedHost': {
+            'level': 'ERROR',
+            'handlers': ['console', 'mail_admins'],
+            'propagate': True
+        }
+    }
+}
+"""
---
+++ config/urls.py
@@ -0,0 +1,29 @@
+"""Main URLs module."""
+
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.contrib import admin
+
+
+urlpatterns = [
+    # Django Admin
+    path(settings.ADMIN_URL, admin.site.urls),
+
+    # Core app
+    path('', include(('apps.core.urls', 'core'), namespace='core')),
+
+    # Auth
+    path('accounts/', include('django.contrib.auth.urls')), 
+    path('accounts/', include(('apps.registration.urls', 'registration'), namespace='registration')),
+
+    # Profiles
+    path('profiles/', include(('apps.profiles.urls', 'profiles'), namespace='profiles')),
+
+    # Inventories
+    path('inventories/', include(('apps.inventories.urls', 'inventories'), namespace='inventories')), 
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
